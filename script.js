1816bf90476a23c412c6c1b6e9853421e2b96ff0
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2024-03-16 16:04:19 +0700

    Hari 3 bg 2

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
 
+// sr.reveal('.about, .ket',{}); 
+sr.reveal('.about, .ket',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
